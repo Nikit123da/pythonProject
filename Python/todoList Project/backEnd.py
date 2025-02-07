de6563a153f589a6f9d86fc35c3a353ec0f0ddc9
Павел Рябцев <rybzbn@gmail.com>
2025-02-07 15:54:40 +0200

    added a window of login and sing up, now working on the animation to switch between them when the password in the sign up window match

--- Python/todoList Project/backEnd.py
+++ Python/todoList Project/backEnd.py
@@ -3,8 +3,11 @@ import eel
 eel.init("C:\\Users\\97254\\Desktop\\Michlala\\Python\\todoList Project\\web")
 
 @eel.expose
-def say_hello(name):
-    print(f"Hello from Python, {name}!")
-    return f"Hello, {name}! This is Python."
+#TODO: MAKE THE checkPassword func work again.
+#TODO: make the log in card fly from the kright to the middle.
+def checkPassword(param1, param2):
+    if param1 == param2:
+        return True
+    return False
 
-eel.start("mainWindow.html", size=(600, 400),port=8000)
+eel.start("signUpPage.html", size=(1000, 1000),port=8000)
--- Python/todoList Project/web/script.js
+++ Python/todoList Project/web/script.js
@@ -7,3 +7,29 @@ function callPython() {
     }
 );
 }
+
+async function checkPassword() { //async funcs are great because they wait for a spesific outside function to finish before they return an answear
+    let password = document.getElementById("password").value;
+    let passwordCheck = document.getElementById("passwordCheck").value;
+
+    
+    let isMatch = await eel.checkPassword(password, passwordCheck)();
+
+    if (isMatch) {
+        document.getElementById("signUpContainer").classList.add("slide-out");
+
+        setTimeout(() => {
+            window.location.href = "loginPage.html";
+        }, 500);
+
+        setTimeout(() => {
+            document.getElementById("loginContainer").classList.add("slide-in");
+        }, 1000); 
+
+
+
+        
+    } else {
+        console.log("Passwords do not match");
+    }
+}
