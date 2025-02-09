81f65e8d505ce8d8a3404dca7652b4e286ccdd08
Павел Рябцев <rybzbn@gmail.com>
2025-02-09 09:51:06 +0200

    ugh

--- Python/todoList Project/backEnd.py
+++ Python/todoList Project/backEnd.py
@@ -8,15 +8,6 @@ eel.init("C:\\Users\\97254\\Desktop\\Michlala\\Python\\todoList Project\\web")
 @eel.expose
 
 def checkPassword(param1, param2):
-    if param1 == param2:
-        return True
-    return False
+    return param1 == param2
 
-
-#mydb = mysql.connector.connect
-#(
-    host = "localhost:3306",
-    user = "root",
-    password = ""
-#)
 eel.start("signUpPage.html", size=(1000, 1000),port=8000)
