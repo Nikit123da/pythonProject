7bbd4a303835fb3a3a205508c779ae3e71ea775f
Павел Рябцев <rybzbn@gmail.com>
2025-03-10 15:27:40 +0200

    new commit

--- FrontEnd/drinksEx/drinks.js
+++ FrontEnd/drinksEx/drinks.js
@@ -6,7 +6,6 @@ let selectedValue = document.getElementById("select");
 
 
 (function addTitles(url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list') {
-    let i = '1', j = '1';
     fetch(url)
         .then(response => response.json()) 
         .then(data => {
@@ -29,20 +28,18 @@ function filterDrinks(selectedValue)
         data.drinks.forEach(element =>
         {   
             if(element.strIngredient+i != null)
-                {
-                    ingridientsArr.push(element.strIngredient+i);
-                    i++;
-                }
+            {
+                ingridientsArr.push(element.strIngredient+i);
+                i++;
+            }
 
-                if(element.strMeasure+j != null)
-                {
-                    messurmentsArr.push(element.strMeasure+j)
-                    j++;
-                }
-            
+            if(element.strMeasure+j != null)
+            {
+                messurmentsArr.push(element.strMeasure+j)
+                j++;
+            }
 
-                
-            buildCard(ingridientsArr, messurmentsArr,element.strDrinkThumb,element.strInstructions,element.strDrink);
+            putCard(ingridientsArr, messurmentsArr,element.strDrinkThumb,element.strInstructions,element.strDrink);
         }
         )
     )
@@ -57,70 +54,38 @@ function addToSearch(value)
     select.appendChild(option);
 }
 
-function buildCard(ingridientsArr, messurmentsArr, imgLink, instructions, drinkName)
+function putCard(ingridientsArr, messurmentsArr, imgLink, instructions, drinkName)
 {
-
-
-    let card = document.createElement("card");
-    let img = document.createElement("img");
-    let textDiv = document.createElement("div");
-    let text = document.createElement("p");
-    let header = document.createElement("h2");
-    let table = createTable(ingridientsArr, messurmentsArr);
-
-    header.innerHTML = drinkName;
-    img.innerHTML = imgLink;
-    text.innerHTML = instructions;
-
-    card.appendChild(header);
-    card.appendChild(img);
-    card.appendChild(textDiv);
-    textDiv.appendChild(text);
-    card.appendChild(table);
-
+    let card = document.querySelectorAll(".main");
+    let drink = document.querySelector(".drinkName");
+    let image = document.querySelector(".img");
+    let instr = document.querySelector(".instructions");
+    let table = fillTable(ingridientsArr, messurmentsArr);
+    instr.innerHTML = instructions;
+    image.src = imgLink;
+    drink.innerHTML = drinkName;
     console.log(card); 
-    
     drinkWindow.appendChild(card);
 }
 
-function createTable(ingridientsArr, messurmentsArr)
+function fillTable(ingridientsArr, messurmentsArr)
 {
-    let table = document.createElement("table");
-
-    //creates the parameters of the table
-    let tableHead = document.createElement("thead");
-    let tableBody = document.createElement("tbody");
-    let headRow = document.createElement("tr");
-    let ingridients = document.createElement("th");
-    let messurments = document.createElement("th");
-
-    table.appendChild(tableHead);
-    table.appendChild(tableBody);
-    //adds the table Head Row 
-    tableHead.appendChild(headRow);
-    headRow.appendChild(ingridients);
-    headRow.appendChild(messurments);
-
-    //changes the values of the table head row
-    ingridients.innerHTML = "Ingridients";
-    messurments.innerHTML = "messurments";
-
-    //loop for the ingridients and messures
     //creates one row at a time with the messurment and ingridient
+    let tableBody = document.querySelectorAll(".tableBody");
+
     let i = 0;
     for(; i < ingridientsArr.length; i++)
     {
-        //creates row
-        let bodyRow = document.createElement("tr");
-        tableBody.appendChild(bodyRow);
+        let row = document.createElement("tr")
+        tableBody.appendChild(row);
         let ing = document.createElement("td");
         let mes = document.createElement("td");
-        bodyRow.appendChild(ing);
-        bodyRow.appendChild(mes);
-
         //updates row's values
         ing.innerHTML = ingridientsArr[i];
         mes.innerHTML = messurmentsArr[i];
+
+        bodyRow.appendChild(ing);
+        bodyRow.appendChild(mes);
     }
 
     return table;
